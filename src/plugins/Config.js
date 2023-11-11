469c8f9676ff0081441e12dadce2f1a7c73c9ad7
ktxxt <ridicdarko@gmail.com>
2023-11-11 16:58:57 +0100

    feat: Add possibility of global config per product

--- src/plugins/Config.js
+++ src/plugins/Config.js
@@ -1,8 +1,8 @@
 const fs = require('fs-extra')
 const path = require('path')
+const _ = require('lodash')
 
 const defaultEnvironment = 'staging'
-const defaultType = 'default'
 
 class Config {
   constructor(on, config, pluginConfig) {
@@ -23,6 +23,9 @@ class Config {
   init(on, config, pluginConfig) {
     this.finalConfig = {}
 
+    /**
+     * Save original config just for reference
+     */
     const configClone = JSON.parse(JSON.stringify(config.env))
     const originalConfig = {
       product: configClone.product,
@@ -32,28 +35,72 @@ class Config {
 
     let { product, env, type } = config.env
 
+    /**
+     * Make sure there is a default environment
+     */
     if (!env) {
       env = defaultEnvironment
     }
 
-    if (!type) {
-      type = defaultType
+    /**
+     * Load component global config
+     */
+    this.finalConfig = require(path.resolve('.', 'cypress/configs', `${product}/global.ts`)).default
+
+    /**
+     * Load test type config and merge with the above one
+     *
+     * In the CLI this is defined with `--type`
+     */
+    if (type) {
+      this.finalConfig = _.merge(
+        this.finalConfig,
+        require(path.resolve('.', 'cypress/configs', `${product}/${type}.ts`)).default,
+      )
     }
 
-    this.finalConfig = require(path.resolve('.', 'cypress/configs', `${product}/${type}.ts`)).default
+    /**
+     * Load local config if exists
+     */
+    try {
+      const myConfigPath = path.resolve('.', `cypress/configs/${product}/`, 'cypress.local.ts')
+      if (fs.existsSync(myConfigPath)) {
+        const myConfig = require(myConfigPath).default
 
+        this.finalConfig = _.merge(this.finalConfig, myConfig)
+
+        if (this.CONFIG.logging && Object.keys(myConfig).length) {
+          console.log(`\n[Plugin:Config]: Local config found.`, this.finalConfig)
+        }
+      }
+    } catch (error) {
+      console.error(error)
+    }
+
+    /**
+     * Precaution as `e2e` needs to be defined
+     */
     if (!this.finalConfig.e2e) {
       this.finalConfig.e2e = {}
     }
 
-    if (!this.finalConfig.specPattern || !this.finalConfig.specPattern.length) {
-      this.finalConfig.specPattern = [`cypress/e2e/${product}/**/*`]
+    /**
+     * If no specs are defined, load up every products spec file
+     */
+    if (!this.finalConfig.e2e.specPattern || !this.finalConfig.e2e.specPattern.length) {
+      this.finalConfig.e2e.specPattern = [`cypress/e2e/${product}/**/*`]
     }
 
+    /**
+     * Set the `env` prop if it's not set
+     */
     if (!this.finalConfig.env) {
       this.finalConfig.env = {}
     }
 
+    /**
+     * Set PRODUCT, ENV, and TYPE for easier references when they're needed in the runtime
+     */
     if (!this.finalConfig.env.PRODUCT) {
       this.finalConfig.env.PRODUCT = product
     }
@@ -66,22 +113,13 @@ class Config {
       this.finalConfig.env.TYPE = type
     }
 
-    this.finalConfig.env = {
-      ...this.finalConfig.env,
-      ...config.env,
-    }
-
+    /**
+     * Store the original variables from the main Cypress config into the final config
+     */
     this.finalConfig.env.originalConfig = originalConfig
 
-    if (this.CONFIG.logging) {
-      console.log('[Plugin:Config] Product: ', this.finalConfig.env.PRODUCT)
-      console.log('[Plugin:Config] Environment: ', this.finalConfig.env.ENV)
-      console.log('[Plugin:Config] Type: ', this.finalConfig.env.TYPE)
-      console.log('[Plugin:Config] Original config: ', JSON.stringify(this.finalConfig.env.originalConfig))
-    }
-
     /**
-     * Copy baseUrl from fixtures of the current product, environment into final config
+     * Copy baseUrl from fixtures of the current product, and environment into final config
      */
     const routes = require(`${path.resolve('.', 'cypress/fixtures/', `${product}/routes.json`)}`)
 
@@ -89,25 +127,15 @@ class Config {
       this.finalConfig.e2e.baseUrl = routes.envs[env].baseUrl
     }
 
-    try {
-      const myConfigPath = path.resolve('.', 'cypress/configs/', 'cypress.local.ts')
-      if (fs.existsSync(myConfigPath)) {
-        this.finalConfig = {
-          ...this.finalConfig,
-          ...require(myConfigPath).default,
-        }
-
-        if (this.CONFIG.logging) {
-          console.log(`\n[Plugin:Config]: Local config found.`)
-        }
-      }
-    } catch (error) {
-      console.error(error)
-    }
-
     if (this.CONFIG.logging) {
-      console.log('[Plugin:Config] Config set:', this.finalConfig)
+      console.log('[Plugin:Config] Product: ', this.finalConfig.env.PRODUCT)
+      console.log('[Plugin:Config] Environment: ', this.finalConfig.env.ENV)
+      console.log('[Plugin:Config] Type: ', this.finalConfig.env.TYPE)
+      console.log('[Plugin:Config] Original env config: ', JSON.stringify(this.finalConfig.env.originalConfig))
+      console.log('[Plugin:Config] Config overrides: \n', this.finalConfig)
     }
+
+    this.finalConfig = _.merge(config, this.finalConfig)
   }
 }
 
